5621eddf76dbfe390b6649be5ce0be079f00d64c
miracleworks <contactmw@gmail.com>
2015-03-15 12:07:29 -0400

    Initial

---
+++ Playgrounds/Variables.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+var firstInteger:Int = 0, secondInteger:Int = 29
+var thirdFloat:Float = -1000858
+var x:Int = 5
+
+// I will be using this later to track the discounts at this department store - it will change seasonly
+var discountAtDepartmentStore:Double = 0.3
+
+
+/*
+Mutliline comment
+first line comment
+sencond line comment
+*/
+
+
+
+
+
+
+
+
+
+
+
+
